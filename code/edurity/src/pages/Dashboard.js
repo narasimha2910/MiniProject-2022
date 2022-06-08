91e4de12c8a6160f8bb199acf5648ffc25e20d45
Pruthvi Raaj <pruthviraaj@Pruthvis-MacBook-Pro.local>
2022-06-08 23:39:27 +0530

    created dashboard

---
+++ code/edurity/src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer";
+import NavBar_dashboard from "../components/NavBar_Dashboard";
+import Table_Dashboard from "../components/Table_Dashboard";
+
+
+const Dashboard = () => {
+  return (
+    <>
+      <NavBar_dashboard />
+
+      <Table_Dashboard />
+
+      <Footer />
+    </>
+  );
+};
+
+export default Dashboard;
--- code/edurity/src/routes/App.js
+++ code/edurity/src/routes/App.js
@@ -1,9 +1,11 @@
 import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
   return (
     <div className=" bg-opacity-20 bg-blockchain bg-cover w-screen h-screen">
-      <Home />
+      
+      <Dashboard />
     </div>
   );
 }
