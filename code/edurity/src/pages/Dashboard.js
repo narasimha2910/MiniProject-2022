8edc1ea18337222867d22a592af6736c9f335329
narasimha2910 <2910.narasimha@gmail.com>
2022-06-08 23:53:14 +0530

    Merged changes

--- code/edurity/src/pages/Dashboard.js
+++ code/edurity/src/pages/Dashboard.js
@@ -1,15 +1,17 @@
-
 import Table_Dashboard from "../components/Table_Dashboard";
 import React from "react";
-import  { useEffect } from "react";
+import { useEffect } from "react";
 
 const Dashboard = ({ setActive }) => {
   useEffect(() => {
     setActive(2);
   }, [setActive]);
 
-  return <> <Table_Dashboard /> </>;
- 
+  return (
+    <>
+      <Table_Dashboard />
+    </>
+  );
 };
 
 export default Dashboard;
